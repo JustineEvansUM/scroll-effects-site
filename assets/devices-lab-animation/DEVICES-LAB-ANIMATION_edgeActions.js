52bb6d154f71e37eddd2e9c216ea64bd16de3815
Justine Evans <justine.evans@umontana.edu>
2017-10-10 08:04:58 -0600

    Initialized repo.

---
+++ assets/devices-lab-animation/DEVICES-LAB-ANIMATION_edgeActions.js
@@ -0,0 +1,8 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",11592,function(sym,e){});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+})(jQuery,AdobeEdge,"EDGE-322814474");
